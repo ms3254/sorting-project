08cf2d302a5c14dce411e5ed0f7ad66f968ce173
ms3254 <mdvshah19@gmail.com>
2025-01-28 05:39:06 +0000

    added random array

--- Main.java
+++ Main.java
@@ -10,19 +10,19 @@ public class Main {
         //  3. Provide random input to test suite and ensure your implementations
         //     of InsertionSort and Selection sort continue to work.
         //  4. Increase the loop count for TestSuite to get better performance results.
-        /*public static int[] arrayCreator(){
-            int[] newArr = new int[(int)(Math.random() *10)+1];
 
-            for (int i=0; i<newArr.length;i++){
-                newArr[i] = (int) (Math.random()*10)+1;
-            }
-            return newArr;
-        }*/
-
-        int[] testInput = { 1, 2, 3, 1 };
+        int[] testInput = arrayCreator();
 
         // Increase the loop count to get better results once it works.
         // TestSuite.run(testInput, 10000);
         TestSuite.run(testInput, 10);
+    }
+    public static int[] arrayCreator(){
+        int[] newArr = new int[(int)(Math.random() *10)+1];
+
+        for (int i=0; i<newArr.length;i++){
+            newArr[i] = (int) (Math.random()*10)+1;
+        }
+        return newArr;
     }
 }
